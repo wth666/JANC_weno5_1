4565fc1dd37fcd3ad4ebfa96eaaf5bc146d29865
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-20 13:20:17 +0800

    Update pressure_outlet.py

--- src/janc/boundary/pressure_outlet.py
+++ src/janc/boundary/pressure_outlet.py
@@ -11,13 +11,13 @@ def pressure_outlet(state_out,gamma_out,T_out,normal_vel,Pb):
     p_out = rho_out*(R_out*T_out)
     a_out = jnp.sqrt(gamma_out*p_out/rho_out)
     mask = (normal_vel/a_out < 1)
-    #rho_cor_out = jax.lax.select(mask, Pb / (p_out / rho_out),rho_out)
-    #p_cor_out = jax.lax.select(mask, Pb*jnp.ones_like(p_out),p_out)
-    #T_cor_out = jax.lax.select(mask, p_cor_out/(rho_cor_out*R_out),T_out)
+    rho_cor_out = jax.lax.select(mask, Pb / (p_out / rho_out),rho_out)
+    p_cor_out = jax.lax.select(mask, Pb*jnp.ones_like(p_out),p_out)
+    T_cor_out = jax.lax.select(mask, p_cor_out/(rho_cor_out*R_out),T_out)
     
-    p_cor_out = p_out
-    T_cor_out = T_out
-    rho_cor_out = rho_out
+    #p_cor_out = p_out
+    #T_cor_out = T_out
+    #rho_cor_out = rho_out
     
     _, gamma_out, h_out, _, _ = thermo.get_thermo(T_cor_out,Y_out)
     U_bd = jnp.concatenate([rho_cor_out, rho_cor_out * u_out, rho_cor_out * v_out,
